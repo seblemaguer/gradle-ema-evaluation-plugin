56fe5f82ddf16a6e71effc85191305238dbe4867
Sebastien Le Maguer <slemaguer@coli.uni-saarland.de>
2017-02-22 07:17:02 +0100

    Adding plugin

---
+++ src/main/groovy/de/dfki/mary/emaevaluation/EMAEvaluationPlugin.groovy
@@ -0,0 +1,139 @@
+package de.dfki.mary.emaevaluation
+
+
+import org.gradle.api.JavaVersion
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.api.logging.LogLevel
+import org.gradle.api.plugins.JavaPlugin
+import org.gradle.api.plugins.MavenPlugin
+import org.gradle.api.tasks.Copy
+import org.gradle.api.tasks.Exec
+import org.gradle.api.tasks.JavaExec
+import org.gradle.api.tasks.bundling.Zip
+
+import static groovyx.gpars.GParsPool.runForkJoin
+import static groovyx.gpars.GParsPool.withPool
+
+import groovy.json.JsonBuilder
+import groovy.json.JsonSlurper
+
+import marytts.analysis.utils.LoadingHelpers;
+import marytts.analysis.Statistics;
+
+import de.dfki.mary.emaevaluation.subparts.*
+
+class EMAEvaluationPlugin implements Plugin<Project>
+{
+    @Override
+    void apply(Project project)
+    {
+        project.plugins.apply JavaPlugin
+        project.plugins.apply MavenPlugin
+
+        project.sourceCompatibility = JavaVersion.VERSION_1_7
+
+
+        // Load configuration
+        def slurper = new JsonSlurper()
+        def config_file = project.rootProject.ext.config_file
+        def config = slurper.parseText( config_file.text )
+
+        // Adapt pathes
+        DataFileFinder.project_path = new File(getClass().protectionDomain.codeSource.location.path).parent
+        if (config.data.project_dir) {
+            DataFileFinder.project_path = config.data.project_dir
+        }
+
+        // See for number of processes for parallel mode
+        def nb_proc_local = 1
+        if (project.gradle.startParameter.getMaxWorkerCount() != 0) {
+            nb_proc_local = Runtime.getRuntime().availableProcessors(); // By default the number of core
+            if (config.settings.nb_proc) {
+                if (config.settings.nb_proc > nb_proc_local) {
+                    throw Exception("You will overload your machine, preventing stop !")
+                }
+
+                nb_proc_local = config.settings.nb_proc
+            }
+        }
+
+        (new File(project.rootProject.buildDir.toString() + "/EMAAnalysis")).mkdirs()
+
+        project.ext {
+            // User configuration
+            user_configuration = config;
+
+            emaOutputDir = new File(project.rootProject.buildDir.toString() + "/EMAAnalysis")
+
+            // FIXME: externalize that !
+            list_file = new File(DataFileFinder.getFilePath("list_test"))
+            referenceDir = ["ema": "../extraction/build/ema"]
+            synthesizeDir = ["ema": "../synthesis/build/output/imposed_dur"]
+            channels = ["T3", "T2", "T1", "ref", "jaw", "upperlip", "lowerlip"]
+
+            nb_proc = nb_proc_local;
+
+            loading = new LoadingHelpers();
+        }
+
+        project.status = project.version.endsWith('SNAPSHOT') ? 'integration' : 'release'
+
+        project.repositories {
+            jcenter()
+            maven {
+                url 'http://oss.jfrog.org/artifactory/repo'
+            }
+        }
+
+        project.configurations.create 'legacy'
+
+        project.sourceSets {
+            main {
+                java {
+                    // srcDir project.generatedSrcDir
+                }
+            }
+            test {
+                java {
+                    // srcDir project.generatedTestSrcDir
+                }
+            }
+        }
+
+        project.afterEvaluate {
+            (new EMAAnalysis()).addTasks(project)
+
+
+            project.task("generateEMAReport") {
+                dependsOn "computeRMSEEMA", "computeEucDistEMA"
+
+
+                def input_rms_ema = new File("${project.emaOutputDir}/rms_ema.csv")
+
+
+                def output_f = new File("${project.emaOutputDir}/global_report.csv")
+                outputs.files output_f
+
+                doLast {
+
+                    output_f.text = "#id\tmean\tstd\tconfint\n"
+
+                    // RMS F0 part
+                    values = []
+                    input_rms_ema.eachLine { line ->
+                        if (line.startsWith("#"))
+                            return; // Continue...
+
+                            def elts = line.split()
+                            values << Double.parseDouble(elts[1])
+                    }
+                    dist = new Double[values.size()];
+                    values.toArray(dist);
+                    s = new Statistics(dist);
+                    output_f << "rms ema\t" << s.mean() << "\t" << s.stddev() << "\t" << s.confint(0.05) << "\n"
+                }
+            }
+        }
+    }
+}
--- src/main/groovy/de/dfki/mary/emaevaluation/subparts/ema.groovy
+++ src/main/groovy/de/dfki/mary/emaevaluation/subparts/ema.groovy
@@ -5,25 +5,13 @@ import org.gradle.api.Project
 import de.dfki.mary.ttsanalysis.AnalysisInterface
 
 import marytts.analysis.distances.acoustic.*;
+import marytts.analysis.distances.graphic.*;
 import marytts.analysis.alignment.IDAlignment;
 
 class EMAAnalysis implements AnalysisInterface
 {
     public void addTasks(Project project)
     {
-
-        project.task("computeEMARMSE")
-        {
-            doLast {
-                output_f.text = "#id\tmcdist\n"
-
-                project.list_file.eachLine { line ->
-
-
-                    def nb_frames = Math.min(src.length, tgt.length)
-                }
-            }
-        }
 
         project.task("computeRMSEEMA") {
 
@@ -34,32 +22,31 @@ class EMAAnalysis implements AnalysisInterface
             doLast {
                 output_f.text = "#id\trmse (mm)\n"
 
-                list_file.eachLine { line ->
+                project.list_file.eachLine { line ->
                     // Load files
                     double[][] src =
                         project.loading.loadFloatBinary("${project.referenceDir['ema']}/${line}.ema",
-                                                        self.channels.size()*3); // FIXME: hardcoded frame size
+                                                        project.channels.size()*3); // FIXME: hardcoded frame size
                     double[][] tgt =
                         project.loading.loadFloatBinary("${project.synthesizeDir['ema']}/${line}.ema",
-                                                        self.channels.size()*3); // FIXME: hardcoded frame size
+                                                        project.channels.size()*3); // FIXME: hardcoded frame size
 
                     def nb_frames = Math.min(src.length, tgt.length)
 
 
                     // Compute and dump the distance
                     def alignment = new IDAlignment(nb_frames);
-                    def v = new RMS(src, tgt, self.channels.size()*3); // FIXME: hardcoded frame size
-                    def v = new CepstralDistorsion(src, tgt, project.mgcDim);
+                    def v = new RMS(src, tgt, project.channels.size()*3); // FIXME: hardcoded frame size
                     Double d = v.distancePerUtterance(alignment);
                     output_f << "$line\t$d\n";
                 }
             }
         }
 
-        task computeEucDistEMA() {
+        project.task("computeEucDistEMA") {
 
             def output_handles = []
-            self.channels.each { c ->
+            project.channels.each { c ->
                 output_handles << new File("${project.emaOutputDir}/euc_dist_${c}.csv")
                 outputs.files output_handles
             }
@@ -68,19 +55,18 @@ class EMAAnalysis implements AnalysisInterface
                 output_handles.each {f ->
                     f.text = "# euc. dist. (cm)\n"
                 }
-                def loading = new LoadingHelpers();
-                list_file.eachLine { line ->
+                project.list_file.eachLine { line ->
                     // Load files
                     double[][] src =
                         project.loading.loadFloatBinary("${project.referenceDir['ema']}/${line}.ema",
-                                                        self.channels.size()*3); // FIXME: hardcoded frame size
+                                                        project.channels.size()*3); // FIXME: hardcoded frame size
                     double[][] tgt =
                         project.loading.loadFloatBinary("${project.synthesizeDir['ema']}/${line}.ema",
-                                                        self.channels.size()*3); // FIXME: hardcoded frame size
+                                                        project.channels.size()*3); // FIXME: hardcoded frame size
 
 
                     int nb_frames = Math.min(src.length, tgt.length)
-                    for (int j=0; j<self.channels.size()*3; j+=3)  // FIXME: hardcoded frame size
+                    for (int j=0; j<project.channels.size()*3; j+=3)  // FIXME: hardcoded frame size
                     {
                         double[][] real_src = new double[nb_frames][3];
                         double[][] real_tgt = new double[nb_frames][3];
